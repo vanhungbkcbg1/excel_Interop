ef36494f7d6d40668b86ea2ac5dbb73e7127bcc5
vanhung <vanhungbkcbg1@gmail.com>
2016-07-10 14:41:34 +0700

    add bad method export

--- WindowsApplication1/WindowsApplication1/Form1.Designer.vb
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -23,22 +23,33 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(62, 52)
+        Me.Button1.Location = New System.Drawing.Point(37, 52)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(100, 40)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Create File Good"
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(143, 52)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(91, 40)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Create File Bad"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -46,5 +57,6 @@ Partial Class Form1
 
     End Sub
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
 
 End Class
--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -3,6 +3,7 @@
 Public Class Form1
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim start_time = Now()
         Dim xlApp As Excel.Application = New Microsoft.Office.Interop.Excel.Application()
 
         If xlApp Is Nothing Then
@@ -25,7 +26,7 @@ Public Class Form1
             range.Value = myvalue
         Next index
 
-
+        xlApp.DisplayAlerts = False
         xlWorkBook.SaveAs("d:\csharp-Excel.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, _
          Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue)
         xlWorkBook.Close(True, misValue, misValue)
@@ -34,8 +35,10 @@ Public Class Form1
         releaseObject(xlWorkSheet)
         releaseObject(xlWorkBook)
         releaseObject(xlApp)
+        Dim end_time = Now()
 
-        MessageBox.Show("Excel file created , you can find the file d:\csharp-Excel.xls")
+        Console.WriteLine((end_time - start_time).TotalSeconds())
+        'MessageBox.Show("Excel file created , you can find the file d:\csharp-Excel.xls")
     End Sub
 
     Private Sub releaseObject(ByVal obj As Object)
@@ -47,5 +50,60 @@ Public Class Form1
         Finally
             GC.Collect()
         End Try
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+        Dim start_time = Now()
+
+        Dim xlApp As Excel.Application = New Microsoft.Office.Interop.Excel.Application()
+
+        If xlApp Is Nothing Then
+            MessageBox.Show("Excel is not properly installed!!")
+            Return
+        End If
+
+
+        Dim xlWorkBook As Excel.Workbook
+        Dim xlWorkSheet As Excel.Worksheet
+        Dim misValue As Object = System.Reflection.Missing.Value
+
+        xlWorkBook = xlApp.Workbooks.Add(misValue)
+        xlWorkSheet = xlWorkBook.Sheets("sheet1")
+        'xlWorkSheet.Cells(1, 1) = "Sheet 1 content"
+
+        For index As Integer = 1 To 1000
+            'Dim myvalue As String() = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"}
+            'Dim range = xlWorkSheet.Range(String.Format("A{0}", index), String.Format("L{0}", index))
+            'range.Value = myvalue
+            xlWorkSheet.Cells(index, 1) = "Sheet 1 content"
+            xlWorkSheet.Cells(index, 2) = "Sheet 1 content"
+            xlWorkSheet.Cells(index, 3) = "Sheet 1 content"
+            xlWorkSheet.Cells(index, 4) = "Sheet 1 content"
+            xlWorkSheet.Cells(index, 5) = "Sheet 1 content"
+            xlWorkSheet.Cells(index, 6) = "Sheet 1 content"
+            xlWorkSheet.Cells(index, 7) = "Sheet 1 content"
+            xlWorkSheet.Cells(index, 8) = "Sheet 1 content"
+            xlWorkSheet.Cells(index, 9) = "Sheet 1 content"
+            xlWorkSheet.Cells(index, 10) = "Sheet 1 content"
+            xlWorkSheet.Cells(index, 11) = "Sheet 1 content"
+            xlWorkSheet.Cells(index, 12) = "Sheet 1 content"
+        Next index
+
+        xlApp.DisplayAlerts = False
+        xlWorkBook.SaveAs("d:\csharp-Excel.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, _
+         Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue)
+        xlWorkBook.Close(True, misValue, misValue)
+        xlApp.Quit()
+
+        releaseObject(xlWorkSheet)
+        releaseObject(xlWorkBook)
+        releaseObject(xlApp)
+
+        Dim end_time = Now()
+
+        Console.WriteLine((end_time - start_time).TotalSeconds())
+
+        'MessageBox.Show("Excel file created , you can find the file d:\csharp-Excel.xls")
     End Sub
 End Class
